98ab1dfb6db50f0814a93d9e6b7dccc646e5b119
kutyel <flaviocorpa@gmail.com>
2017-11-13 15:51:43 +0100

    Implement first functor 🤓

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 'use strict'
-module.exports = {}
+
+// Our first functor! 🎉
+const Box = x => ({
+  map: f => Box(f(x)),
+  fold: f => f(x),
+  inspect: () => `Box(${x})`,
+})
+
+const nextCharFromNumberString = str =>
+  Box(str)
+    .map(s => s.trim())
+    .map(parseInt)
+    .map(i => i + 1)
+    .map(String.fromCharCode)
+    .fold(c => c.toLowerCase())
+
+const result = nextCharFromNumberString('  64 ')
+
+console.log(result) // 'a'
+
+module.exports = Box

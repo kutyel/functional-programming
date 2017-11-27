9105cfcb0bba8d273ffac9eaad2fff7c7e460d90
kutyel <flaviocorpa@gmail.com>
2017-11-27 08:42:37 +0100

    Split monoids from semigroups

---
+++ monoids.js
@@ -0,0 +1,27 @@
+const Product = x => ({
+  x,
+  concat: ({ x: y }) => Product(x * y)
+})
+
+Product.empty = () => Product(1)
+
+const Any = x => ({
+  x,
+  concat: ({ x: y }) => Any(x || y)
+})
+
+Any.empty = () => Any(false)
+
+const Max = x => ({
+  x,
+  concat: ({ x: y }) => Max(x > y ? x : y)
+})
+
+Max.empty = () => Max(-Infinity)
+
+const Min = x => ({
+  x,
+  concat: ({ x: y }) => Min(x < y ? x : y)
+})
+
+Min.empty = () => Min(Infinity)
